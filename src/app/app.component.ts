502f3cd65f5324589751a43a72a1190c6282c822
jmdiaz2 <jmdiaz2@esei.uvigo.es>
2022-05-26 11:34:04 +0200

    feat: list of agencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jm-astro-bookings';
+  public title = 'Astro Bookings';
+  public subtitle = 'Welcome on board';
+  public author = 'Juan Moure';
+  public authorUrl = 'http://spadejj.com';
+  public agencies = [
+    {
+      id: 'space-y',
+      name: 'Space Y',
+      range: 'Interplanetary',
+      status: 'Active',
+    },
+    {
+      id: 'green-origin',
+      name: 'Green Origin',
+      range: 'Orbital',
+      status: 'Active',
+    },
+    {
+      id: 'virgin-way',
+      name: 'Virgin Way',
+      range: 'Orbital',
+      status: 'Pending',
+    },
+  ];
+  public reloading = false;
+
+
+  public getAgenciesLength(){
+    return this.agencies.length;
+  }
+
+  public reload(list: string){
+    this.reloading = true;
+    console.log("Reloading..." + list);
+  }
+
 }
