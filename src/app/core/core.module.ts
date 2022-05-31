cbc7187d91fa30493cc2738178ea22260ce6ed9d
jmdiaz2 <jmdiaz2@esei.uvigo.es>
2022-05-31 10:36:41 +0200

    footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { TitleComponent } from './title/title.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    TitleComponent
+    TitleComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class CoreModule { }
