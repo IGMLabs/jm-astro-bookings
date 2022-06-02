df001223f1520b0604ccde02b31a545275ab5d8c
jmdiaz2 <jmdiaz2@esei.uvigo.es>
2022-06-02 11:27:19 +0200

    agency

---
+++ src/app/agencies/agency/agency.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agency',
+  templateUrl: './agency.page.html',
+  styleUrls: ['./agency.page.css']
+})
+export class AgencyPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
