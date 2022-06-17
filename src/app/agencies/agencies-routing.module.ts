e464505e91a70c88b0556e5adebf30bc509789c6
jmdiaz2 <jmdiaz2@esei.uvigo.es>
2022-06-17 10:10:14 +0200

    auth guard created

--- src/app/agencies/agencies-routing.module.ts
+++ src/app/agencies/agencies-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticatedGuard } from '../auth/api/authenticated.guard';
 import { AgenciesPage } from './agencies.page';
 
 const routes: Routes = [
   { path: '', component: AgenciesPage },
-  { path: 'agency/new', loadChildren: () => import('./new-agency/new-agency.module').then(m => m.NewAgencyModule) },
+  { path: 'agency/new', canLoad:[AuthenticatedGuard], loadChildren: () => import('./new-agency/new-agency.module').then(m => m.NewAgencyModule) },
   { path: 'agency/:id', loadChildren: () => import('./agency/agency.module').then(m => m.AgencyModule) }
 ];
 
